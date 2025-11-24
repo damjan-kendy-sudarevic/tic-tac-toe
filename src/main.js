77e5034f1e3878bd1a0ab19f1672ee55adff577a
Damjan Sudarević <sudarevic.damjan@gmail.com>
2025-11-25 00:19:44 +0100

    Game board and cell initialization

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
 import './style.css';
+
+/* Constants */
+const ROWS = 3;
+const COLS = 3;
+
+/* Utility Functions */
+function createCell() {
+	const CellElement = document.createElement('span');
+	CellElement.classList.add('cell');
+	CellElement.value = null;
+	return CellElement;
+}
+
+function createBoard(root) {
+	const BoardElement = document.createElement('div');
+	BoardElement.classList.add('board');
+
+	const Board = new Array(ROWS * COLS).fill(null).map(createCell);
+	Board.forEach(cell => BoardElement.appendChild(cell));
+
+	root.appendChild(BoardElement);
+	return Board;
+}
+
+/* Main */
+const GameContainer = document.querySelector('#game');
+const Board = createBoard(GameContainer);
+console.log(Board);
